9041db19a9f294053dc45dbd1503f2edfb48055b
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-07-07 19:46:43 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//Define UI Vars
+
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+//Load all event listeners
+loadEventListeners();
+
+//Load all event listeners
+function loadEventListeners() {
+  //Add task event
+  form.addEventListener("submit", addTask);
+}
+
+//Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task to proceed");
+  }
+
+  e.preventDefault();
+}
+console.log("test");
